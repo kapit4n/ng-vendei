1b1e8731c6fa7e2701e38c9bc0222dd0704fa345
lartl <luis.arce22@gmail.com>
2018-09-18 07:48:00 -0400

    Integrate add item on shopping cart

--- src/app/components/vendei/product-list/product-list.component.ts
+++ src/app/components/vendei/product-list/product-list.component.ts
@@ -7,15 +7,21 @@ import { VProductListService } from '../../../services/vendei/v-product-list.ser
   styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
-  @Input() selected: any[];
+  @Input()
+  selectedProducts: any[];
   products = [];
   constructor(private pListSvc: VProductListService) {}
 
   ngOnInit() {
-    this.pListSvc.getProducts().subscribe(res => this.products = res);
+    this.pListSvc.getProducts().subscribe(res => (this.products = res));
   }
 
   addProduct(product: any) {
-    console.log(product);
+    if (this.selectedProducts.some(p => p.id == product.id))  {
+      this.selectedProducts.filter(p => p.id == product.id)[0].quantity += 1;
+    } else {
+      const selectedP = Object.assign({ quantity: 1 }, product);
+      this.selectedProducts.push(selectedP);
+    }
   }
 }
