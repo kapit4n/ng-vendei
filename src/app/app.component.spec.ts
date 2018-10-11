524a5d357b4cc06052d672950e250bdd88596db8
lartl <luis.arce22@gmail.com>
2018-10-11 08:16:36 -0400

    Fixing app test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,81 @@
 import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { APP_BASE_HREF } from "@angular/common";
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/vendei/page-not-found/page-not-found.component';
+
+import { MainScreenshotComponent } from './components/vendei/main-screenshot/main-screenshot.component';
+import { ProductListComponent } from './components/vendei/product-list/product-list.component';
+import { ShoppingCartComponent } from './components/vendei/shopping-cart/shopping-cart.component';
+import { CalTableComponent } from './components/vendei/cal-table/cal-table.component';
+import { SelectedListComponent, SelectedProductEditDialog } from './components/vendei/selected-list/selected-list.component';
+import { VProductListService } from './services/vendei/v-product-list.service';
+
+
+const appRoutes: Routes = [
+  { path: "mock", component: MainScreenshotComponent },
+  {
+    path: "main",
+    component: ShoppingCartComponent,
+    data: { title: "Shopping Cart" }
+  },
+  {
+    path: "",
+    redirectTo: "/main",
+    pathMatch: "full"
+  },
+  { path: "**", component: PageNotFoundComponent }
+];
+
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent,
+        ProductListComponent,
+        MainScreenshotComponent,
+        PageNotFoundComponent,
+        ShoppingCartComponent,
+        CalTableComponent,
+        SelectedListComponent,
+        SelectedProductEditDialog
       ],
+      imports: [
+        RouterModule.forRoot(appRoutes, { enableTracing: true }),
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatCardModule,
+        MatInputModule,
+        MatGridListModule,
+        MatIconModule,
+        MatListModule,
+        MatTableModule,
+        MatDialogModule,
+        FormsModule
+      ],
+      providers: [VProductListService, { provide: APP_BASE_HREF, useValue: '/' }],
+      entryComponents: [SelectedProductEditDialog]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
