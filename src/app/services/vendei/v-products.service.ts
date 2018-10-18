b5eec8e7f8bbf456dd837738ce705db0bab9993d
lartl <luis.arce22@gmail.com>
2018-10-18 06:45:47 -0400

    Renaming products service

--- src/app/services/vendei/v-products.service.ts
+++ src/app/services/vendei/v-products.service.ts
@@ -9,46 +9,46 @@ import 'rxjs'; //get everything from Rx
 @Injectable({
   providedIn: 'root'
 })
-export class VProductListService {
+export class VProductsService {
   /** Product list */
   products: any[];
-  
+
   /** json URL */
-	private jsonFileURL: string = "../../assets/vendei/products.json";
+  private jsonFileURL: string = "../../assets/vendei/products.json";
 
   /** Product List service constructor */
   constructor(private http: HttpClient) {
-	}
+  }
 
   /** 
    * Returns the list of products
    */
-	list(): any[] {
-		return this.products;
-	}
+  list(): any[] {
+    return this.products;
+  }
 
   /**
    * Return Product by category
    */
-	productsByCategory(category: any): any[] {
-		return this.products;
-	}
+  productsByCategory(category: any): any[] {
+    return this.products;
+  }
 
   /**
    * Return an observable with the yeam that matches the id
    */
-	getProductById(id: any): Observable<any> {
-		return this.http.get(this.jsonFileURL).pipe(map((response: Response) => {
-			return <any>response.json()[id - 1]
-		}));
-	}
+  getProductById(id: any): Observable<any> {
+    return this.http.get(this.jsonFileURL).pipe(map((response: Response) => {
+      return <any>response.json()[id - 1]
+    }));
+  }
 
   /**
    * Return an observable with the list of products
    */
-	getProducts(): Observable<any> {
-		return this.http.get(this.jsonFileURL).pipe(map((response: Response) => {
-			return <any>response
-		}));
-	}
+  getProducts(): Observable<any> {
+    return this.http.get(this.jsonFileURL).pipe(map((response: Response) => {
+      return <any>response
+    }));
+  }
 }
