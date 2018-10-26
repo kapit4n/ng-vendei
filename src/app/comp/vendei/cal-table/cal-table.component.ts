df54cfe77d3cc0ac21defb81e19b4e4554aad338
lartl <luis.arce22@gmail.com>
2018-10-25 20:17:50 -0400

    Display paymed, returned and discount items

--- src/app/comp/vendei/cal-table/cal-table.component.ts
+++ src/app/comp/vendei/cal-table/cal-table.component.ts
@@ -14,27 +14,74 @@ enum PaymentType {
   styleUrls: ["./cal-table.component.css"]
 })
 export class CalTableComponent implements OnInit {
-  @Input() selectCustomer: Function;
-  @Input() selectedCustomer: any;
+  @Input()
+  selectCustomer: Function;
+  @Input()
+  selectedCustomer: any;
 
   bills = [
-    { name: "0.1", value: 0.1 },
-    { name: "0.2", value: 0.2 },
-    { name: "0.5", value: 0.5 },
-    { name: "1", value: 1 },
-    { name: "2", value: 2 },
-    { name: "5", value: 5 },
-    { name: "10", value: 10 }
+    { name: "0.1", value: 0.1, img: "" },
+    { name: "0.2", value: 0.2, img: "" },
+    {
+      name: "0.5",
+      value: 0.5,
+      img: "https://en.numista.com/catalogue/photos/bolivie/g397.jpg"
+    },
+    {
+      name: "1",
+      value: 1,
+      img:
+        "https://i.ucoin.net/coin/15/048/15048242-1/bolivia-1-boliviano-2012.jpg"
+    },
+    {
+      name: "2",
+      value: 2,
+      img: "https://i.colnect.net/f/4353/166/2-Bolivianos-Large-Type.jpg"
+    },
+    {
+      name: "5",
+      value: 5,
+      img: "https://i.colnect.net/f/2815/611/5-Bolivianos.jpg"
+    },
+    {
+      name: "10",
+      value: 10,
+      img:
+        "http://www.lostiempos.com/sites/default/files/styles/noticia_detalle/public/media_imagen/2018/4/4/sin_titulo-1_0.jpg?itok=xuBQscCf"
+    },
+    {
+      name: "20",
+      value: 20,
+      img: "https://media2.allnumis.com/1189_572233078c3986fL.jpg"
+    },
+    {
+      name: "50",
+      value: 50,
+      img:
+        "https://img.ma-shops.com/aurich/pic/14169_bolivien_50_bolivianos_28.11.1986_bn_.jpg"
+    },
+    {
+      name: "100",
+      value: 100,
+      img:
+        "https://www.paginasiete.bo/u/fotografias/m/2017/9/27/f800x450-180473_231919_0.jpg"
+    },
+    {
+      name: "200",
+      value: 200,
+      img:
+        "https://www.eldeber.com.bo/__export/1481083317747/sites/eldeber/img/2015/07/02/5595aa693d5f4.jpeg_1775534641.jpeg"
+    }
   ];
 
   numbers = [
-    { name: "1", value: 1 },
-    { name: "2", value: 2 },
-    { name: "3", value: 3 },
-    { name: "4", value: 4 },
-    { name: "5", value: 5 },
-    { name: "6", value: 6 },
-    { name: "7", value: 7 }
+    { name: "1", value: 1, img: "" },
+    { name: "2", value: 2, img: "" },
+    { name: "3", value: 3, img: "" },
+    { name: "4", value: 4, img: "" },
+    { name: "5", value: 5, img: "" },
+    { name: "6", value: 6, img: "" },
+    { name: "7", value: 7, img: "" }
   ];
 
   payItems: Array<any>;
@@ -50,13 +97,16 @@ export class CalTableComponent implements OnInit {
 
   animal: string;
   name: string;
-  
+
   constructor(public dialog: MatDialog) {
     this.payType = PaymentType.PAYMONEY;
     this.payItems = this.bills;
     this.displayCurrentType = true;
-  }
 
+    this.payedItems = [];
+    this.discountItems = [];
+    this.returnItems = [];
+  }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(CustomersDialogComponent, {
@@ -73,9 +123,7 @@ export class CalTableComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   payMoney() {
     this.displayCurrentType = true;
@@ -95,9 +143,9 @@ export class CalTableComponent implements OnInit {
     this.payType = PaymentType.PAYRETURN;
   }
 
-  payIt(payItem: PaymentType) {
-
-    switch (payItem) {
+  payIt(payItem: any) {
+    console.log(payItem);
+    switch (this.payType) {
       case PaymentType.PAYMONEY:
         this.payedItems.push(payItem);
         break;
@@ -110,5 +158,16 @@ export class CalTableComponent implements OnInit {
       default:
         break;
     }
+
+    let totalPayment = this.payedItems.map(x => x.value).reduce((a, b) => a + b, 0);
+    console.log("total is " + totalPayment);
+    
+    let totalReturn = this.returnItems.map(x => x.value).reduce((a, b) => a + b, 0);
+    console.log("total is " + totalReturn);
+
+    let totalDiscount = this.discountItems.map(x => x.value).reduce((a, b) => a + b, 0);
+    console.log("total is " + totalDiscount);
+
+
   }
 }
